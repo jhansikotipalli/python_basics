543a740f84038cb1bb92634736dcb862a79297f8
jhansi kotipalli <jhansicdac@gmil.com>
2026-02-23 19:03:22 +0530

    new

---
+++ strings/string.py
@@ -0,0 +1,15 @@
+#//strings//
+msg="  Hello, World  "
+print(msg[5])
+print(msg[::-1])
+
+print(msg.replace("World", "Python"))
+print(msg.lower())
+print(msg.upper())
+print(msg.find("o"))
+print(msg.strip())
+print(msg.split(","))
+print("l1\nl2")
+name="akash"
+age=24
+print("i am {} and {} years old".format(name,age))
